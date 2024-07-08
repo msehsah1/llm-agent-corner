a266a34b1e1f6b2c62132cc249c8f2b2c114675a
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-07-08 13:43:07 -0400

    updating the requirements.txt

---
+++ main/frontEndComponents/agents_interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+
+class Agent(ABC):
+    def __init__(self):
+        pass
+
+    def agent_init(self):
+        pass
+
+    def agent_wrapper(self):
+        pass
