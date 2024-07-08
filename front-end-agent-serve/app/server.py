7f2f0ac017fc109c838b6265e9539b3b86d479e7
Mahmoud Sehsah <46302138+msehsah1@users.noreply.github.com>
2024-07-08 00:59:42 +0000

    init branch

---
+++ front-end-agent-serve/app/server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatAnthropic, ChatOpenAI
+from fastapi.middleware.cors import CORSMiddleware
+from langserve import add_routes
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="A simple api server using Langchain's Runnable interfaces",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+model = ChatOpenAI(model="gpt-3.5-turbo-0125")
+add_routes(
+    app,
+    ChatOpenAI(model="gpt-3.5-turbo-0125"),
+    path="/openai",
+)
+
+prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+add_routes(
+    app,
+    prompt | model,
+    path="/joke",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8100)
