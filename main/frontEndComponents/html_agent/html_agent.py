9d4728c4670484186489ee3e7524f24e29dadedb
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-07-08 17:50:26 -0400

    adding the tools for the html agent the html files creator tool and the folder creator tool

--- main/frontEndComponents/html_agent/html_agent.py
+++ main/frontEndComponents/html_agent/html_agent.py
@@ -1,10 +1,11 @@
 from main.frontEndComponents.agents_interface import Agent
 from main.utils.utils import override
+from main.frontEndComponents.prompt import REACT_AGENT_PROMPT, HTML_PROMPT_INSTRUCTION
 
 
 class HTMLAgent(Agent):
-    def __init__(self):
-        super().__init__(prompt="", tools="")
+    def __init__(self, prompt):
+        super().__init__(prompt=prompt, tools="")
 
     @override
     @classmethod
@@ -15,6 +16,7 @@ class HTMLAgent(Agent):
         :param prompt: prompt for the front end agent (instruction on what it will generate)
         :return: The result of the invoke method
         """
-        html_inst = cls()
+        react_prompt = REACT_AGENT_PROMPT.partial(instruction=HTML_PROMPT_INSTRUCTION)
+        html_inst = cls(react_prompt)
         html_agent_executor = html_inst.agent_init()
         return html_agent_executor.invoke(input=prompt)
--- main/frontEndComponents/html_agent/tools.py
+++ main/frontEndComponents/html_agent/tools.py
@@ -1,1 +1,18 @@
-print("test")
+from langchain_core.tools import Tool
+from main.frontEndComponents.html_agent.tools_funcs import create_html_files_wrapper, create_folder
+
+TOOLS = [
+    Tool(
+        name='HTML files Creator',
+        func=create_html_files_wrapper,
+        description="""This is a tool that takes the html code (str) and a filename (str) from the agent then parse 
+        them and then call the create_html_files function to create a new html file at a specific location. the 
+        function accepts a LIST composed of the HTML content and a filename"""
+    ),
+    Tool(
+        name="Folder Creator",
+        func=create_folder,
+        description="""This is a tool that a foldername and then create a new folder with the foldername in a 
+        specific location."""
+    )
+]
